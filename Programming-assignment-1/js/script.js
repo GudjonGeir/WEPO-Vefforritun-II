2c47c4c9b7056e51e246188dc6c09bded468cd0c
Freysi21 <freysi21@gmail.com>
2015-01-20 18:30:34 +0000

    Pen and Line tool ready

--- Programming-assignment-1/js/script.js
+++ Programming-assignment-1/js/script.js
@@ -1,6 +1,6 @@
 // Constructor for tool settings
 function ToolSettings() {
-	this.shape = "rect";
+	this.shape = "pen";
 	this.color = "#000000";
 }
 
@@ -13,10 +13,56 @@ function Rectangle(x, y, w, h, fill) {
 	this.fill = fill;
 }
 
+function Line(x, y, x2, y2, fill) {
+	this.x = x;
+	this.y = y;
+	this.x2 = x2;
+	this.y2 = y2;
+	this.fill = fill;
+}
+
+function Pen(x, y, fill){
+	this.x = x;
+	this.y = y;
+	this.fill = fill;
+}
+
+Pen.prototype.draw = function(ctx) {
+	for(var i = 0; i < this.x.length - 1; i++) {
+		console.log("i is " + i);
+		ctx.beginPath(); 
+		ctx.fillStyle = this.fill;
+		ctx.moveTo(this.x[i], this.y[i]);
+		ctx.lineTo(this.x[i + 1], this.y[i + 1]);
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
+
+  /*for(var i=0; i < clickX.length; i++) {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+     context.stroke();
+  }*/
+
+Line.prototype.draw = function(ctx) {
+	ctx.beginPath();
+	ctx.fillStyle = this.fill;
+	ctx.moveTo(this.x, this.y);
+	ctx.lineTo(this.x2, this.y2);
+	ctx.stroke();
+}
+
 // Draw the rectangle
 Rectangle.prototype.draw = function(ctx) {
   ctx.fillStyle = this.fill;
-  ctx.fillRect(this.x, this.y, this.w, this.h);
+  ctx.fillRect(this.w, this.h, this.x - this.w, this.y - this.h);
 }
 
 function CanvasState(canvas) {
@@ -67,11 +113,25 @@ $(document).ready(function() {
 		// TODO: implement move
 
 		currentState.isValid = false;
+		currentState.isDrawing = true;
+
 		if(tools.shape === "rect") {
-			currentState.isDrawing = true;
 			currentState.startX = e.pageX - this.offsetLeft;
 			currentState.startY = e.pageY - this.offsetTop;
 			currentState.shapes.push(new Rectangle(currentState.startX, currentState.startY, 0, 0, tools.color));
+		} else if(tools.shape === "line") {
+			currentState.startX = e.pageX - this.offsetLeft;
+			currentState.startY = e.pageY - this.offsetTop;
+			currentState.shapes.push(new Line(currentState.startX, currentState.startY, currentState.startX, currentState.startY, tools.color));
+		} else if(tools.shape === "pen") {
+			console.log(currentState.shapes.length);
+			var x = new Array();
+			var y = new Array();
+			x.push(e.pageX - this.offsetLeft);
+			y.push(e.pageY - this.offsetTop);
+			currentState.shapes.push(new Pen(x, y, tools.color));
+			console.log(currentState.shapes.length);
+
 		}
 	});
 
@@ -79,12 +139,21 @@ $(document).ready(function() {
 		if (currentState.isDrawing) {
 
 			if(tools.shape === "rect") {
-				var x = e.pageX - this.offsetLeft;
-				var y = e.pageY - this.offsetTop;
 				var currentShape = currentState.shapes.pop();
-				currentShape.w = Math.abs(currentState.startX - x);
-				currentShape.h = Math.abs(currentState.startY - y);
+				currentShape.w = e.pageX - this.offsetLeft;
+				currentShape.h = e.pageY - this.offsetTop;
 				currentState.shapes.push(currentShape);
+			} else if(tools.shape === "line") {
+				var currentShape = currentState.shapes.pop();
+				currentShape.x2 = e.pageX - this.offsetLeft;
+				currentShape.y2 = e.pageY - this.offsetTop;
+				currentState.shapes.push(currentShape);
+			} else if(tools.shape === "pen") {
+				var currentShape = currentState.shapes.pop();
+				currentShape.x.push(e.pageX - this.offsetLeft);
+				currentShape.y.push(e.pageY - this.offsetTop);
+				currentState.shapes.push(currentShape);
+				console.log(currentState.shapes.length);
 			}
 			currentState.isValid = false;
 		};
