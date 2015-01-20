464a12641e312bb06143bb53dd906b9d02781dc9
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-01-20 18:38:29 +0000

    Added rudimentary tool selector and implemented circle drawing

--- Programming-assignment-1/js/script.js
+++ Programming-assignment-1/js/script.js
@@ -1,22 +1,56 @@
 // Constructor for tool settings
 function ToolSettings() {
 	this.shape = "rect";
-	this.color = "#000000";
+	this.fill = "#FFFFFF";
+	this.lineWidth = "5";
+	this.stroke = "#000000"
 }
 
 // Constructor for rectangle object
-function Rectangle(x, y, w, h, fill) {
+function Rectangle(x, y, w, h, fill, stroke) {
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
 	this.fill = fill;
+	this.stroke = stroke;
 }
 
 // Draw the rectangle
 Rectangle.prototype.draw = function(ctx) {
   ctx.fillStyle = this.fill;
+  strokeStyle = this.stroke
   ctx.fillRect(this.x, this.y, this.w, this.h);
+  ctx.strokeRect(this.x, this.y, this.w, this.h);
+}
+
+function Circle(x1, y1, x2, y2, fill, lineWidth, stroke) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+	this.fill = fill;
+	this.lineWidth = lineWidth;
+	this.stroke = stroke;
+}
+
+Circle.prototype.draw = function(ctx) {
+	var x, y, rad, a, b;
+	x = (this.x1 + this.x2) / 2;
+	y = (this.y1 + this.y2) / 2;
+
+	a = (Math.abs(this.x1 - this.x2));
+	b = (Math.abs(this.y1 - this.y2));
+	rad = Math.sqrt((a * a) + (b * b)) / 2;
+
+
+	ctx.beginPath()
+	ctx.arc(x, y, rad, 0, 2 * Math.PI, false);
+	ctx.fillStyle = this.fill;
+	ctx.fill();
+	ctx.lineWidth = this.lineWidth;
+	ctx.strokeStyle = this.stroke;
+	ctx.stroke()
 }
 
 function CanvasState(canvas) {
@@ -65,32 +99,63 @@ $(document).ready(function() {
 
 	$("#myCanvas").mousedown(function(e) {
 		// TODO: implement move
+		var x, y;
 
 		currentState.isValid = false;
+		currentState.isDrawing = true;
+		currentState.startX = e.pageX - this.offsetLeft;
+		currentState.startY = e.pageY - this.offsetTop;
+		x = currentState.startX;
+		y = currentState.startY;
+
 		if(tools.shape === "rect") {
-			currentState.isDrawing = true;
-			currentState.startX = e.pageX - this.offsetLeft;
-			currentState.startY = e.pageY - this.offsetTop;
-			currentState.shapes.push(new Rectangle(currentState.startX, currentState.startY, 0, 0, tools.color));
-		}
+			currentState.shapes.push(new Rectangle(x, y, 0, 0, tools.fill, tools.stroke));
+		} else if (tools.shape === "circle") {
+			currentState.shapes.push(new Circle(x, y, x, y, tools.fill, tools.lineWidth, tools.stroke));
+		};
 	});
 
 	$("#myCanvas").mousemove(function(e) {
 		if (currentState.isDrawing) {
+			var currentShape;
+			var x = e.pageX - this.offsetLeft;
+			var y = e.pageY - this.offsetTop;
 
-			if(tools.shape === "rect") {
-				var x = e.pageX - this.offsetLeft;
-				var y = e.pageY - this.offsetTop;
-				var currentShape = currentState.shapes.pop();
+			if(tools.shape === "rect") {				
+				currentShape = currentState.shapes.pop();
 				currentShape.w = Math.abs(currentState.startX - x);
 				currentShape.h = Math.abs(currentState.startY - y);
-				currentState.shapes.push(currentShape);
+			} else if (tools.shape === "circle") {
+				currentShape = currentState.shapes.pop();
+				currentShape.x2 = x;
+				currentShape.y2 = y;
+
+			} else {
+				return;
 			}
+			currentState.shapes.push(currentShape);
 			currentState.isValid = false;
 		};
 	});
 
 	$("#myCanvas").mouseup(function(e) {
 		currentState.isDrawing = false;
+	});
+
+
+	$(".tool").click(function(e) {
+		tools.shape = $(this).data("tool");
+	});
+
+	$(".strokeColor").click(function(e) {
+		tools.stroke = $(this).data("strokecolor");
+	});
+
+	$(".fillColor").click(function(e) {
+		tools.fill = $(this).data("fillcolor");
+	});
+
+	$(".line").click(function(e) {
+		tools.lineWidth = $(this).data("strokewidth");
 	});
 });
