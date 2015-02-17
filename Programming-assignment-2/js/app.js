5861600edb5393e74ff151ac12a28f56f75ceebb
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-02-17 17:38:30 +0000

    Still getting started

--- Programming-assignment-2/js/app.js
+++ Programming-assignment-2/js/app.js
@@ -3,15 +3,40 @@ var ChatterClient = angular.module("ChatterClient", ['ngRoute']);
 ChatterClient.config(
 	function ($routeProvider) {
 		$routeProvider
-			.when("/login", { templateUrl: "views/login.html", controller: "LoginController"})
+			.when("/login", { templateUrl: "views/login.html", controller: "LoginController" })
+			.when("/roomlist/:user", {templateUrl: "views/rooms.html", controller: "RoomListController" })
 			.otherwise({
 				redirectTo: "/login"
 			});
 	});
 
 ChatterClient.controller("LoginController", function ($scope, $location, $rootScope, $routeParams, socket) {
-	$scope.bla = "";
+	$scope.username = "";
+	$scope.errorMessage = "";
+	$scope.displayError = false;
 	$scope.login = function() {
-		$scope.bla = $scope.username;
-	}
+		if ($scope.username === "") {
+			$scope.errorMessage = "Please choose a username";
+			$scope.displayError = true;
+		}
+		else {
+			socket.emit('adduser', $scope.username, function (available) {
+				if (available) {
+					$location.path("/roomlist/" + $scope.username);
+				}
+				else {
+					$scope.errorMessage = "Selected username is already taken";
+					$scope.displayError = true;
+				}
+			});
+		}
+	};
+});
+
+ChatterClient.controller("RoomListController", function ($scope, $location, $rootScope, $routeParams, socket) {
+	$scope.currentUser = $routeParams.user;
+	socket.emit("rooms");
+	socket.on("roomlist", function (roomList) {
+		$scope.roomList = roomList;
+	});
 });
--- Programming-assignment-2/js/socket-factory.js
+++ Programming-assignment-2/js/socket-factory.js
@@ -20,7 +20,7 @@ ChatterClient.factory('socket', function ($rootScope) {
                         callback.apply(socket, args);
                     }
                 });
-            })
+            });
         }
     };
 });
